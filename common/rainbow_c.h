797d3da2802e4c8ffa61792fcd7b71d92bfdace8
Matheus Bittencourt <bittencourt.matheus@gmail.com>
2019-03-27 17:31:36 -0300

    rainbow-eta: smaller key sizes for GF(256) projects

--- common/rainbow_c.h
+++ common/rainbow_c.h
@@ -38,8 +38,35 @@ struct _rainbow_key {
 
 typedef struct _rainbow_key rainbow_key;
 
+// smaller private key for rainbow-eta
+
+struct _rainbow_small_ckey {
+	uint8_t l2_o[_O2 * _O2];
+	uint8_t l2_vo[_O2][_V2 * _O2];
+	uint8_t l2_vv[TERMS_QUAD_POLY(_V2) * _O2];
+};
+
+typedef struct _rainbow_small_ckey rainbow_small_ckey;
+
+struct _rainbow_small_key {
+	uint8_t mat_t[_SEC_N * _PUB_N_BYTE];
+	uint8_t vec_t[_PUB_N_BYTE];
+	uint8_t mat_s[_PUB_M * _PUB_M_BYTE];
+	uint8_t vec_s[_PUB_M_BYTE];
+
+	uint8_t vinegar[_V1];
+	uint8_t mat_l1[_O1 * _O1];
+	uint8_t temp_vv1[_O1];
+
+	rainbow_small_ckey ckey;
+};
+
+typedef struct _rainbow_small_key rainbow_small_key;
+
 /// length for secret key ( extra 1 for length of salt)
 #define _SEC_KEY_LEN (sizeof(rainbow_key) + 1)
+
+#define SMALL_SEC_KEY_LEN (sizeof(rainbow_small_key) + 1)
 
 /// algotithm 6
 void rainbow_genkey(uint8_t* pk, uint8_t* sk);
--- rainbow-genkey.c
+++ rainbow-genkey.c
@@ -9,7 +9,6 @@
 #include "api.h"
 
 #ifndef SMALL_SEC_KEY_LEN
-#error rainbow-eta not implemented for these parameters
 #define SMALL_SEC_KEY_LEN CRYPTO_SECRETKEYBYTES
 #endif
 
